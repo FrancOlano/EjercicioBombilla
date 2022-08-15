7cc2b31c9286f4f9cd1303445e7e1710401c967b
olanofranco1@gmail.com <FrancOlano>
2022-08-15 17:27:47 +0200

    ej19

--- Ejercicio bombilla/main.js
+++ Ejercicio bombilla/main.js
@@ -1,6 +1,27 @@
-const btn = document.getElementById('btn')
-const light = document.getElementById('light')
+const btn = document.getElementById("btn");
+const light = document.getElementById("light");
+var toggle = true, width = light.width, height = light.height;
+const controls = document.getElementById("controls");
+const zoom = document.getElementById("zoom");
+const level = document.getElementById("level");
 
-btn.onclick = function () {
-    
-}
+btn.addEventListener("click", function () {
+  toggle = !toggle;
+  if (toggle) {
+    btn.setAttribute("src", "./boff.jpg");
+    light.setAttribute("src", "./off.jpg");
+    controls.style.display = "none";
+  } else {
+    btn.setAttribute("src", "./bon.jpg");
+    light.setAttribute("src", "./on.jpg");
+    controls.style.display = "flex";
+  }
+});
+
+level.addEventListener("input", function () {
+  light.style.opacity = level.value / 100;
+});
+zoom.addEventListener("input", function () {
+    light.width = width * (zoom.value / 100);
+    light.height = height * (zoom.value / 100);
+  });
